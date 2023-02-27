c021645c6fa9edff55c5641dd59011ad2008bf21
Mvrs <johnsonmarlon18@gmail.com>
2023-02-27 12:22:41 -0800

    init commit to ui-kit branch

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Marlon Johnson on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
